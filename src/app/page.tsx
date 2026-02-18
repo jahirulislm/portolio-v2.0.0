fb39a9ab13cf0de87061e2ca4ad1d2ab11471ef0
jahirulislm <jahirulislam.dev@gmail.com>
2026-02-18 19:15:45 +0600

    remove shoppage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
         <Projects />
         {/* <Skills /> */}
         {/* <Blog /> */}
-        <DigitalGoods />
+        {/* <DigitalGoods /> */}
         <Process />
         <Contact />
         <ReadyToShip />
